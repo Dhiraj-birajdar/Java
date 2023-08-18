e54fec9400ce9882cf5f5093193002d556afcffe
Dhiraj Birajdar <birajdar257@gmail.com>
2023-08-18 19:24:48 +0530

    float digit acuracy unicode to char view

---
+++ S2/BitView.java
@@ -0,0 +1,18 @@
+class BitView
+{
+    public static void main(String[] args)
+    {
+        byte a = -128;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toOctalString(a));
+        System.out.println(Integer.toHexString(a));
+
+        float f = 1.1234567f;//accurate upto 8 digit
+        double b = 1.012345678901234;//upto 16 digit
+        System.out.println(b);
+
+        char c = 0x30;
+        System.out.println(c);
+        System.out.println(0x30);
+    }
+}
