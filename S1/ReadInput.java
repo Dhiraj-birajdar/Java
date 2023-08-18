033d70caa8886c74f91c584b3a612953563cb22a
Dhiraj Birajdar <birajdar257@gmail.com>
2023-08-18 22:39:41 +0530

    reading input

---
+++ S1/ReadInput.java
@@ -0,0 +1,29 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class ReadInput
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter name : ");
+        boolean name = s.hasNextInt();
+        System.out.println("hello "+name);
+        
+    }
+}
+
+/*
+nextByte();
+nextShort();
+nextInt();
+nextLong();
+nextFloat();
+nextDouble();
+next();       to get single word
+nextLine();   to get whole line
+nextBoolean();
+
+hasNextInt();  returns true if input is int else false
+hasNextFloat();  returns true if input is float else false
+*/
