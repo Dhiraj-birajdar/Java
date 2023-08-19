fd51bac479c533a1293b78b1c9287538d8cee047
Dhiraj Birajdar <birajdar257@gmail.com>
2023-08-19 22:48:20 +0530

    3

---
+++ S5/Expr.java
@@ -0,0 +1,15 @@
+class Expr
+{
+    public static void main(String[] args)
+    {
+        char a = 'A', c = 65535 ;
+        byte b = 127;
+        float f = 3;
+        double d = 'a';
+        double k = (short)25l;
+        int i = 3;
+        short s = 32767;
+        long l = 99999999999999999l;
+        System.out.println(d);
+    }
+}
