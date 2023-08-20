94c3401eeb23e97e58c6180852444acb210f6125
Dhiraj Birajdar <birajdar257@gmail.com>
2023-08-20 12:24:59 +0530

    area of triangle

--- S5/TriArea.java
+++ S5/TriArea.java
@@ -1,15 +1,41 @@
 import java.util.Scanner;
+// import java.Math.sqrt;
 
 class TriArea
 {
+    
     public static void main(String[] args)
     {
-        float b, h, a;
+        BaseHeight bh = new BaseHeight();
+        Sides si = new Sides();
+        System.out.println("Area of triangle is "+bh.area()+"\t"+si.area());
+    }
+}
+class BaseHeight
+{
+    float area()
+    {
+        float b, h;
         Scanner s = new Scanner(System.in);
         System.out.println("Enter base and height of triangle respectively : ");
         b = s.nextFloat();
         h = s.nextFloat();
-        a = (1.0f/2)*b*h;
-        System.out.println("Area of triangle is "+a);
+        return (1f/2)*b*h;
+        // System.out.println("Area of triangle is "+a);
+    }
+}
+class Sides
+{
+    float area()
+    {
+        float a, b, c, sum;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter sides a b c of triangle respectively : ");
+        a = s.nextFloat();
+        b = s.nextFloat();
+        c = s.nextFloat();
+        sum = (a+b+c)/2f;
+        return (float)Math.sqrt(sum*(sum-a)*(sum-b)*(sum-c));
+        // System.out.println("Area of triangle is "+a);
     }
 }
