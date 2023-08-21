9ed155e50b182c6403004433211be28d9f8ff968
Dhiraj Birajdar <birajdar257@gmail.com>
2023-08-21 22:32:47 +0530

    print

---
+++ S6/Print.java
@@ -0,0 +1,10 @@
+class Print
+{
+    public static void main(String[] args)
+    {
+        System.out.print("hello");
+        System.out.println("hello");
+        System.out.printf("hello");
+        System.out.format("hello");
+    }
+}
