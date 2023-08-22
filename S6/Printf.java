a8d5448a96e843a0b60b51fa58422815c6478f49
Dhiraj Birajdar <birajdar257@gmail.com>
2023-08-22 20:22:28 +0530

    printf flag, arg index, width

--- S6/Printf.java
+++ S6/Printf.java
@@ -3,6 +3,7 @@ class Printf
     public static void main(String[] args)
     {
         System.out.printf("%2$d  %1$d ",1,2);
-        System.out.format("%2$d  %1$d ",1,2);
+        System.out.format("%2$ 6.2f  %1$(5d ",-2,3.1439);
+        System.out.format("%2$-10s  %1$+5d ",-2,"dhiraj");
     }
 }
